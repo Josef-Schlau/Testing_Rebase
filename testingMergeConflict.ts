b44037e266f01de60284aa407f428c0e375d56cf
josefschlaumeier <josefschlaumeier@web.de>
2022-03-28 18:20:12 +0200

    first commit

---
+++ testingMergeConflict.ts
@@ -0,0 +1,10 @@
+function testingMergeConflict(x: number)
+{
+    let tests = {pd: 2, sg: "df"};
+}
+
+function testingMergeConflict2(y: string)
+{
+    let tests = {pd: 234, sg: "dfsfdsf"};
+    return y;
+}
